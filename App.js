a4cb551edd7e8b4a91cde79a0c0329a2eb9526f4
vPokladLI <viktor.p@li.solutions>
2022-02-18 11:44:49 +0200

    regscreen

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import AuthForm from "./src/Components/Forms/AuthForm";
-import * as Font from 'expo-font';
-import {AppLoading} from "expo"
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
-const image = require("./assets/images/background.jpeg");
-const loadApplication = async () =>{
-  await Font.loadAsync({"Roboto-reg" : require('./assets/fonts/Roboto-Regular.ttf')})
-}
+const image = require("./assets/images/background.jpg");
+const loadApplication = async () => {
+  await Font.loadAsync({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+};
 
-const App = () => (
-  <View style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Hello!</Text>
-      <AuthForm style={styles.authForm} />
-    </ImageBackground>
-  </View>
-);
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onFinish={() => {
+          setIsReady(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Hello!</Text>
+        <AuthForm style={styles.authForm} />
+      </ImageBackground>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -33,7 +53,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     backgroundColor: "#000000c0",
-    marginBottom: 30
+    marginBottom: 30,
+    fontFamily: "Roboto-reg",
   },
 });
 
